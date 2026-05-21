98100ac79afe5fcf9b54d7b604a46a84ea030c9a
CodeNomad-01 <danielmoralestoro05@gmail.com>
2026-05-20 23:22:53 -0500

    taller

--- taller2/ecuaciones.py
+++ taller2/ecuaciones.py
@@ -1,26 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Para resolver el problema y' = f(t, y) (y escalar o vector numpy)
 def Euler(f, a, b, h, co):
-    n = int((b-a) / h)
+    n = int((b - a) / h)
     y_euler = [co]
-
     for i in range(n):
-        y_euler.append(y_euler[i]+h*f(a+i*h, y_euler[i]))
-    return np.linspace(a, b, n+1), y_euler
-
-def f(t, y):
-    w = y[0]
-    z = y[1]
-    f1 = z
-    f2 = t*np.exp(2*t) - 6*z - 9*w
-    return np.array([f1, f2])
-
-t, sol = Euler(f, 0, 1, 0.25, np.array([0, 1]))
-y_sol = np.array(sol)[:,0]
-y_prime = np.array(sol)[:,1]
-
-# print(t)
-# print(sol)
-print(y_sol)
-print(y_prime)
+        y_euler.append(y_euler[i] + h * f(a + i*h, y_euler[i]))
+    return np.linspace(a, b, n + 1), y_euler
